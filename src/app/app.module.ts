255e56afcb3e2ee25204013db1dcac9031c37346
Diego137-ux <62929033+Diego137-ux@users.noreply.github.com>
2022-09-24 12:00:01 -0500

    angular primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
